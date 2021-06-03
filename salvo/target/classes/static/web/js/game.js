207512efee135ee8e7befa4e7fed04ee3a7eb1bf
marcelo verdejo <marcelo.vrdj@gmail.com>
2021-06-02 20:23:43 -0400

    task 4

--- salvo/target/classes/static/web/js/game.js
+++ salvo/target/classes/static/web/js/game.js
@@ -1,61 +1,61 @@
-$(function() {
-  // load and display JSON sent by server for /players
-    const urlParams = new URLSearchParams(window.location.search);
-    $.get("/api/game_view/" + urlParams.get('gp')).done(function(data){
-            var item=document.createElement('tr');
-            var item2=document.createElement('td');
-            item2.appendChild(document.createTextNode(" "));
-            item.appendChild(item2);
-            var res = [];
-            data.ships.forEach(function (dt){
-                res = res.concat(dt.locations.map(s=>s.split("")));
-            });
-            var salv = [];
-           data.salvo.forEach(function (dt){
-               salv = salv.concat(dt.locations.map(s=>s.split("")));
-           });
-
-            for(var i=1;i<=10;i++){
-                item2= document.createElement('td');
-                item2.appendChild(document.createTextNode(i));
-                item.appendChild(item2);
-            }
-            document.getElementById('table1').appendChild(item);
-            for(var y=1;y<=10;y++){
-                item=document.createElement('tr');
-                item2=document.createElement('td');
-                item2.appendChild(document.createTextNode((y+9).toString(36).toUpperCase()));
-                item.appendChild(item2);
-                for(var x=1;x<=10;x++){
-                        item2= document.createElement('td');
-                        res.forEach(function(ele){
-                            if((ele[0].localeCompare((y+9).toString(36).toUpperCase())==0)&&(ele[1].localeCompare(x.toString())==0)){
-                                item2.appendChild(document.createTextNode("F"));
-                                item2.style.backgroundColor = "#0000FF";
-                            }else{
-                                item2.appendChild(document.createTextNode(" "));
-                            }
-                        });
-                        salv.forEach(function(ele){
-                                                    if((ele[0].localeCompare((y+9).toString(36).toUpperCase())==0)&&(ele[1].localeCompare(x.toString())==0)){
-                                                        item2.appendChild(document.createTextNode("G"));
-                                                        item2.style.backgroundColor = "#D35400";
-                                                    }else{
-                                                        item2.appendChild(document.createTextNode(" "));
-                                                    }
-                                                });
-                        item.appendChild(item2);
-                }
-                document.getElementById('table1').appendChild(item);
-            }
-            document.getElementById("para").innerHTML = data.gamePlayers.map(function(p) {
-                var aux = p.player.userName;
-                if(0 == urlParams.get('gp').localeCompare(p.id.toString())){
-                    aux= aux.concat(" (YOU)");
-                }
-                return aux;
-            }).join(" vs ");
-    }).fail(function() {
-            $("#para").text("No se ha encontrado el jugador deseado");
-          });
+$(function () {
+  loadData();
 });
+
+function getParameterByName(name) {
+  var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+function loadData() {
+  $.get('/api/game_view/' + getParameterByName('gp'))
+    .done(function (data) {
+      var playerInfo;
+      if (data.gamePlayers[0].id == getParameterByName('gp'))
+        playerInfo = [data.gamePlayers[0].player, data.gamePlayers[1].player];
+      else
+        playerInfo = [data.gamePlayers[1].player, data.gamePlayers[0].player];
+
+      $('#playerInfo').text(playerInfo[0].userName + '(you) vs ' + playerInfo[1].userName);
+
+      data.ships.forEach(function (shipPiece) {
+        shipPiece.locations.forEach(function (shipLocation) {
+          if(isHit(shipLocation,data.salvoes,playerInfo[0].id)  !=  0){
+            $('#B_' + shipLocation).addClass('ship-piece-hited');
+            $('#B_' + shipLocation).text(isHit(shipLocation,data.salvoes,playerInfo[0].id));
+          }
+
+          else
+            $('#B_' + shipLocation).addClass('ship-piece');
+        });
+      });
+      data.salvoes.forEach(function (salvo) {
+        if (playerInfo[0].id === salvo.player) {
+          salvo.locations.forEach(function (location) {
+            $('#S_' + location).addClass('salvo-piece');
+            $('#S_' + location).text(salvo.turn);
+
+          });
+        } else {
+          salvo.locations.forEach(function (location) {
+            $('#B_' + location).addClass('salvo');
+          });
+        }
+      });
+    })
+    .fail(function (jqXHR, textStatus) {
+      alert('Failed: ' + textStatus);
+    });
+}
+
+function isHit(shipLocation,salvoes,playerId) {
+  var turn = 0;
+  salvoes.forEach(function (salvo) {
+    if(salvo.player != playerId)
+      salvo.locations.forEach(function (location) {
+        if(shipLocation === location)
+          turn = salvo.turn;
+      });
+  });
+  return turn;
+}
